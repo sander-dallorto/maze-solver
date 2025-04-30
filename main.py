986434f8c1e16e2ed5f21d630029cd99bcaee93c
sander-dallorto <sanderrodriguesr75@gmail.com>
2025-04-30 16:33:45 -0300

    Adding maze cells

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from window import Window
 from lines import *
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    win.draw_line(Line(Point(100, 100), Point(200,200)))
-    win.draw_line(Line(Point(200, 100), Point(100,200)), fill_color="red")
     win.wait_for_close()
 
 if __name__ == "__main__":
